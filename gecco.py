a9608d9517d0fa92b5013ee7b3cf50d6d74fb36f
Maarten van Gompel <proycon@anaproy.nl>
2015-01-20 12:50:06 +0100

    skeleton update, multithreading implementation

--- gecco.py
+++ gecco.py
@@ -14,16 +14,42 @@ from threading import Thread, Queue, Lock
 from pynlpl.formats import folia
 from ucto import Tokenizer
 
-import import argparse
+import argparse
 
 UCTOSEARCHDIRS = ('/usr/local/etc/ucto','/etc/ucto/','.')
 
 
-def processor(queue, foliadoc, parameters):
+def processor(queue, lock, data, parameters):
     while True:
-        job = queue.get()
-        job.run(foliadoc, **parameters)
+        module = queue.get()
         queue.task_done()
+
+
+class ProcessorThread(Thread):
+    def __init__(self, q, lock, loadbalancemaster, **parameters):
+        self.q = q
+        self.lock = lock
+        self.abort = False
+        self.loadbalancemaster = loadbalancemaster
+        self.parameters = parameters
+
+    def run(self):
+        while not self.abort:
+            if not q.empty():
+                module, data = q.get() #data is an instance of module.UNIT
+                if module.local:
+                    module.run(data, lock, **self.parameters)
+                else:
+                    host, port = module.findserver(self.loadbalancemaster)
+                    module.client(data, lock, host, port, **parameters)
+
+    def abort(self):
+        self.abort = True
+
+
+
+
+
 
 class Corrector:
     def __init__(self, id, root=".", **settings):
@@ -33,6 +59,17 @@ class Corrector:
         self.verifysettings()
         self.tokenizer = Tokenizer(self.settings['ucto'])
         self.modules = OrderedDict()
+
+        #Gather servers
+        self.servers = set()
+        for module in self:
+            if not module.local:
+                for host, port in module.settings['servers']:
+                    self.servers.add( (host,port) )
+
+        self.loadbalancemaster = LoadBalanceMaster(self.servers)
+
+        self.units = set( [m.server for m in self] )
 
     def verifysettings():
         if not 'ucto' in self.settings:
@@ -57,6 +94,9 @@ class Corrector:
 
         if not 'threads' in self.settings:
             self.settings['threads'] = 1
+
+        if not 'minpollinterval' in self.settings:
+            self.settings['minpollinterval'] = 30 #30 sec
 
 
 
@@ -108,34 +148,50 @@ class Corrector:
             foliadoc = folia.Document(file=foliadoc)
 
 
-        queue = Queue()
+        self.log("Initialising modules on document") #not parellel, acts on same document anyway, should be very quick
+        for module in self:
+            module.init(foliadoc)
+
+        self.log("Initialising threads")
+
 
         lock = Lock()
+        threads = []
         for i in range(self.settings['threads']):
-            thread = Thread(target=processor,args=[queue, lock, foliadoc, parameters])
+            thread = ProcessorThread(queue, lock, self.loadbalancemaster, **parameters)
             thread.setDaemon(True)
             thread.start()
+            threads.append(thread)
 
-        for module in self:
-            queue.put(module)
 
+        queue = Queue() #data in queue takes the form (module, data), where data is an instance of module.UNIT (a folia document or element)
+
+        if folia.Document in units:
+            self.log("\tQueuing modules handling " + str(type(folia.Document)))
+
+            for module in self:
+                if module.UNIT is folia.Document:
+                    queue.put( (module, foliadoc) )
+
+        for unit in units:
+            if unit is not folia.Document:
+                self.log("\tQueuing modules handling " + str(type(unit)))
+                for data in foliadoc.select(unit):
+                    for module in self:
+                        if module.UNIT is unit:
+                            queue.put( (module, data) )
+
+
+        self.log("Processing all modules....")
         queue.join()
-        #all modules done
 
-        #process results and integrate into FoLiA
+        self.log("Finalising modules on document") #not parellel, acts on same document anyway, should be fairly quick depending on module
         for module in self:
-            if id is None or module.id == id:
-                if module.local:
-                    module.run(foliadoc)
-                else:
-                    module.client(foliadoc)
+            module.finish(foliadoc)
 
-
+        self.log("Processing all modules....")
         #Store FoLiA document
-        if save:
-            if not standalone and statusfile: clam.common.status.write(statusfile, "Saving document",99)
-            errout( "Saving document")
-            doc.save()
+        foliadoc.save()
 
     def main(self):
         #command line tool
@@ -149,7 +205,25 @@ class Corrector:
         pass
 
 
+class LoadBalanceMaster: #will cache thingies
+    def __init__(self, availableservers, minpollinterval):
+        self.availableservers = availableservers
+        self.minpollinterval = minpollinterval
+
+
+    def get(self,servers):
+        """Returns the server from servers with the lowest load"""
+        #TODO
+
+
+class LoadBalanceServer: #Reports load balance back to master
+    pass
+
+
 class Module:
+
+    UNIT = folia.Document #Specifies on type of input tbe module gets. An entire FoLiA document is the default, any smaller structure element can be assigned, such as folia.Sentence or folia.Word . More fine-grained levels usually increase efficiency.
+
     def __init__(self,id, **settings):
         self.id = id
         self.settings = settings
@@ -174,20 +248,43 @@ class Module:
         elif 'models' in self.settings:
             self.models = self.settings['models']
 
+    def findserver(self, loadbalanceserver):
+        """Finds a suitable server for this module"""
+        if self.local:
+            raise Exception("Module is local")
+        elif len(self.settings['servers']) == 1:
+            #Easy, there is only one
+            return self.settings['servers'][0] #2-tuple (host, port)
+        else:
+            #TODO: Do load balancing, find least busy server
+            return loadbalancemaster.get(self.settings['servers'])
 
-    #callbacks:
+
+    ####################### CALLBACKS ###########################
+
+
+    def init(self, foliadoc):
+        """Initialises the module on the document. This method should set all the necessary declarations if they are not already present. It will be called sequentially."""
+
+    def finish(self, foliadoc):
+        """Initialises the module on the document. This method can do post-processing. It will be called sequentially."""
 
     def train(self, **parameters):
         """This method gets invoked by the Corrector to train the model. Override it in your own model, use the input files in self.sources and for each entry create the corresponding file in self.models """
 
-    def run(self, foliadoc, **parameters):
-        """This method gets invoked by the Corrector when it runs locally"""
+    def run(self, data, lock, **parameters):
+        """This method gets invoked by the Corrector when it runs locally."""
 
-    def client(self, foliadoc, host, port, **parameters):
-        """This method gets invoked by the Corrector when it should connect to a remote server, the host and port are passed"""
+    def client(self, data, lock, host, port, **parameters):
+        """This method gets invoked by the Corrector when it should connect to a remote server, the host and port are passed."""
+
+
+
 
     def server(self, port):
         """This methods gets called by the Corrector to start the module's server"""
+
+
 
 
 
