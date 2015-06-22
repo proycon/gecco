3dd469066368bcac7519d44da31d5b9f53ac8481
Maarten van Gompel <proycon@anaproy.nl>
2015-06-22 12:38:35 +0200

    added evaluation to test

--- test.sh
+++ test.sh
@@ -12,9 +12,14 @@ if [ $? -ne 0 ]; then
     echo "Training failed!!!" >&2
 fi
 
-echo "Running system">&2
+echo "Running system on test document">&2
 gecco example.yml run -s threads=1 -p debug=1 --local example/test.txt
 if [ $? -ne 0 ]; then
     echo "Run failed!!!" >&2
 fi
 
+echo "Running and evaluating test document">&2
+if [ $? -ne 0 ]; then
+    gecco example.yml evaluate -s threads=1 -p debug=1 --local example/test.txt /tmp/test.folia.xml example/testreference.folia.xml
+    echo "Evaluate failed!!!" >&2
+fi
