37401593d5804bd8d2387374d9eff7ddb486b9c5
Maarten van Gompel (proycon) <proycon@anaproy.nl>
2015-09-03 19:12:06 +0200

    set a timeout on LineByLineClient

--- gecco/gecco.py
+++ gecco/gecco.py
@@ -796,13 +796,15 @@ class Corrector:
 class LineByLineClient:
     """Simple communication protocol between client and server, newline-delimited"""
 
-    def __init__(self, host, port):
+    def __init__(self, host, port,timeout=120):
         self.host = host
         self.port = port
+        self.timeout = timeout
         self.connected = False
 
     def connect(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(self.timeout)
         self.socket.connect( (self.host,self.port) )
         self.connected = True
 
