42d50ec426059851b8bc01bf13023227effecfee
Maarten van Gompel <proycon@anaproy.nl>
2015-10-14 12:13:48 +0200

    fix

--- gecco/helpers/filters.py
+++ gecco/helpers/filters.py
@@ -1,3 +1,4 @@
+from pynlpl.formats import folia
 
-nonumbers = staticmethod(lambda word: word.cls not in ('NUMBER','DATE','NUMBER-YEAR','CURRENCY','FRACNUMBER'))
+nonumbers = staticmethod(lambda word: not isinstance(word, folia.Word) or (isinstance(word,folia.Word) and word.cls not in ('NUMBER','DATE','NUMBER-YEAR','CURRENCY','FRACNUMBER')))
 hasalpha = staticmethod(lambda word: any( ( c.isalpha() for c in str(word) ) ))
