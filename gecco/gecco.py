20cccd156d3a168a3c3f3d3ca3947ab61abbb913
Maarten van Gompel <proycon@anaproy.nl>
2019-10-18 15:56:06 +0200

    do full_load for yaml

--- gecco/gecco.py
+++ gecco/gecco.py
@@ -402,10 +402,10 @@ class Corrector:
 
     def parseconfig(self,configfile):
         self.configfile = configfile #pylint: disable=attribute-defined-outside-init
-        config = yaml.safe_load(open(configfile,'r',encoding='utf-8').read())
+        config = yaml.full_load(open(configfile,'r',encoding='utf-8').read())
 
         if 'inherit' in config:
-            baseconfig = yaml.safe_load(open(config['inherit'],'r',encoding='utf-8').read())
+            baseconfig = yaml.full_load(open(config['inherit'],'r',encoding='utf-8').read())
             baseconfig.update(config)
             config = baseconfig
 
